4146316c1e253b9d05595c4436b99460ac49fba9
Leonardo Mello <158527393+leocorreamello@users.noreply.github.com>
2024-10-21 15:51:35 -0300

    Incluindo modulo 8 e 9 testando o Push.

---
+++ 8-Condicionais2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _8_Condicionais2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executnado o projeto 8 - Condicionais2");
+            //CTRL+K + D --> para organizar o codigo
+
+            int idadeJoao = 16;
+            int quantidadeDePesosas = 2;
+            //Variavel boleana
+            bool acompanhado = quantidadeDePesosas >= 2;
+
+            if (idadeJoao >= 18 || acompanhado == true)
+            {
+                Console.WriteLine("Pode Entrar!");
+            }
+            else
+            {
+                Console.WriteLine("Não pode Entrar!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 9-Escopo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9_Escopo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executnado o projeto 9 - Escopo");
+            //CTRL+K + D --> para organizar o codigo
+
+            int idadeJoao = 16;
+            bool acompanhado = true;
+            string mensagemAdicional;
+
+            if (acompanhado == true)
+            {
+                mensagemAdicional = "João está acompanhado!";
+            }
+            else
+            {
+                mensagemAdicional = "João não está acompanhado!";
+            }
+
+            if (idadeJoao >= 18 || acompanhado == true)
+            {
+                Console.WriteLine("Pode Entrar!");
+                Console.WriteLine(mensagemAdicional);
+            }
+            else
+            {
+                Console.WriteLine("Não pode Entrar!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
